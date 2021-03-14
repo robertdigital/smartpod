af98f700e16d858ee0ab5cebbc7498bb43ec9f8c
Scott Deardorff <scott.deardorff@utiliware.com>
2021-03-14 08:37:42 -0700

    Added copyright/license info

--- flask/control/control.py
+++ flask/control/control.py
@@ -1,3 +1,23 @@
+"""
+control.py - Flask control server APP that talks to hardware device
+             modules and presents a REST API for devices
+Copyright (C) 2021  Ocean Builders LLC
+
+Author: scott@deardorff.org 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import flask
 import json
 
